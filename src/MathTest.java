675d640f00489be5dd95e4204c20e4e28005e71a
earl-stephens <stephensearl@hotmail.com>
2020-04-25 22:14:46 -0400

    Add remaining files

---
+++ src/MathTest.java
@@ -0,0 +1,24 @@
+public class MathTest {
+    public static void main(String[] args)
+    {
+        int first = 15;
+        int second = 25;
+
+    }
+
+    public static int adder()
+    {
+        int first = 15;
+        int second = 25;
+
+        return first + second;
+    }
+
+    public static int multiplier()
+    {
+        int first = 15;
+        int second = 25;
+
+        return first * second;
+    }
+}
